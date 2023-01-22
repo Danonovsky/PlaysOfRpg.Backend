8f2a7c0eaaaf2483c0a281474bf3515c6a1bef60
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2023-01-22 22:05:20 +0100

    Added scanning for all IEndpoints

--- src/PlaysOfRpg/Program.cs
+++ src/PlaysOfRpg/Program.cs
@@ -1,11 +1,27 @@
+using System.Reflection;
 using Abstractions.Services;
-using Infrastructure.Endpoints;
-using Attribute = CharacterModule.Entities.Attribute;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddEndpoints(typeof(Attribute),typeof(SwaggerEndpoint));
+LoadAssemblies();
+var type = typeof(IEndpoint);
+var types = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(_ => _.GetExportedTypes())
+    .Where(_ => _.IsAssignableTo(type) && _.IsInterface is false)
+    .ToArray();
+builder.Services.AddEndpoints(types);
 
 var app = builder.Build();
 app.UseEndpoints();
 
 app.Run();
+
+void LoadAssemblies()
+{
+    var directory = new DirectoryInfo(AppContext.BaseDirectory);
+    if (!directory.Exists) return;
+    var dllFiles = directory.GetFiles("*.dll");
+    foreach (var fileInfo in dllFiles)
+    {
+        Assembly.LoadFrom(fileInfo.FullName);
+    }
+}
