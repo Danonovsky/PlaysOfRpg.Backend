6a95af64caae0228db1cf9b0e3b831329cc8c7ee
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2023-01-22 16:37:28 +0100

    Db

---
+++ src/Modules/CharacterModule/DAL/CharacterDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.EF;
+using Microsoft.EntityFrameworkCore;
+using Attribute = CharacterModule.Entities.Attribute;
+
+namespace CharacterModule.DAL;
+
+public class CharacterDbContext : RpgDbContext
+{
+    private const string SchemaName = "character";
+    public CharacterDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Attribute> Attributes { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(SchemaName);
+    }
+}
---
+++ src/Shared/Infrastructure/EF/RpgDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Abstractions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EF;
+
+public class RpgDbContext : DbContext
+{
+
+    public RpgDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder) 
+        => base.OnModelCreating(builder);
+
+    public override int SaveChanges()
+    {
+        AddTimestamps();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        AddTimestamps();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void AddTimestamps()
+    {
+        var entities = ChangeTracker.Entries()
+            .Where(x => x.Entity is BaseEntity && x.State is EntityState.Added or EntityState.Modified);
+        foreach (var entity in entities)
+        {
+            if(entity.State == EntityState.Added)
+                ((BaseEntity)entity.Entity).CreatedAt = DateTime.UtcNow;
+            else
+                ((BaseEntity)entity.Entity).ModifiedAt = DateTime.UtcNow;
+        }
+    }
+}
